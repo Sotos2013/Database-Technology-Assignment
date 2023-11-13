16c4e5ab1fb0cc9216541d7202b13cc93579818c
KolzarX <harpgiannis@gmail.com>
2023-11-13 12:27:19 +0200

    login gui

--- src/hotel/management/system/Login.java
+++ src/hotel/management/system/Login.java
@@ -11,24 +11,24 @@ import java.awt.event.*;
 import java.sql.*;
 
 public class Login extends JFrame implements ActionListener{
-    
+  
     JLabel l1,l2;
     JTextField t1;
     JPasswordField t2;
     JButton b1,b2;
 
     Login(){
-
-        super("Login");
+           
+        super("Σύνδεση");
 
         setLayout(null);
 
-        l1 = new JLabel("Username");
-        l1.setBounds(40,20,100,30);
+        l1 = new JLabel("Όνομα Χρήστη:");
+        l1.setBounds(10,20,100,30);
         add(l1);
         
-        l2 = new JLabel("Password");
-        l2.setBounds(40,70,100,30);
+        l2 = new JLabel("Κωδικός Πρόσβασης:");
+        l2.setBounds(10,70,100,30);
         add(l2);
  
         t1=new JTextField();
@@ -39,24 +39,24 @@ public class Login extends JFrame implements ActionListener{
         t2.setBounds(150,70,150,30);
         add(t2);
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("hotel/management/system/icons/second.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("hotel/management/system/icons/keylogin.jpg"));
         Image i2 = i1.getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT);
         ImageIcon i3 =  new ImageIcon(i2);
         JLabel l3 = new JLabel(i3);
-        l3.setBounds(350,10,150,150);
+        l3.setBounds(400,30,150,150);
         add(l3);
 
 
-        b1 = new JButton("Login");
-        b1.setBounds(40,140,120,30);
+        b1 = new JButton("Σύνδεση");
+        b1.setBounds(40,180,120,30);
         b1.setFont(new Font("serif",Font.BOLD,15));
         b1.addActionListener(this);
         b1.setBackground(Color.BLACK);
         b1.setForeground(Color.WHITE);
         add(b1);
 
-        b2=new JButton("Cancel");
-        b2.setBounds(180,140,120,30);
+        b2=new JButton("Ακύρωση");
+        b2.setBounds(180,180,120,30);
         b2.setFont(new Font("serif",Font.BOLD,15));
         b2.setBackground(Color.BLACK);
         b2.setForeground(Color.WHITE);
@@ -70,7 +70,7 @@ public class Login extends JFrame implements ActionListener{
         setVisible(true);
         setSize(600,300);
         setLocation(600,350);
-
+setResizable(false);
     }
 
     public void actionPerformed(ActionEvent ae){
